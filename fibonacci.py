76f721dc96b4e275f4a2ca62f707e3c47fa30f73
Ulrich Kerzel <48988450+ukerzel@users.noreply.github.com>
2022-11-22 15:43:59 +0000

    rename Fibonacci number in file

--- fibonacci.py
+++ fibonacci.py
@@ -4,8 +4,8 @@
 def compute_Fibonacci(n_numbers):
     return_list = [0, 1]
     for i in range(2,n_numbers,1):
-        Fib = return_list[i-1]  + return_list[i-2]
-        return_list.append(Fib)
+        fibonacci_number = return_list[i-1]  + return_list[i-2]
+        return_list.append(fibonacci_number)
     return return_list
 
 
